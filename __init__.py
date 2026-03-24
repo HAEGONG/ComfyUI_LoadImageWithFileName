48f20ad446415965bc9cab32a455271880fb0ed5
HAEGONG <dgr0725@gmail.com>
2026-03-24 15:07:31 +0900

    Init

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for ComfyUI_LoadImageWithFileName."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """ComfyUI-LoadImageWithFileName"""
+__email__ = "dgr0725@gmail.com"
+__version__ = "0.0.1"
+
+from .src.ComfyUI_LoadImageWithFileName.nodes import NODE_CLASS_MAPPINGS
+from .src.ComfyUI_LoadImageWithFileName.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
